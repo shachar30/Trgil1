88dc817cafe1c1322f7efe9678d3b47f570f7082
shachar30 <shachr0@gmail.com>
2015-05-28 14:20:08 +0300

    SD

--- data.js
+++ data.js
@@ -1,40 +1,29 @@
 
 
 
+
 var employees = [
 
-    {"firstName":"Ned", "lastName":"Starks", "booksSold":108, "age":56},
-    {"firstName":"Shlomi", "lastName":"Yosef", "booksSold":98, "age":24},
-    {"firstName":"David", "lastName":"Halfon", "booksSold":87, "age":25},
-    {"firstName":"Jon", "lastName":"Snow", "booksSold":13, "age":30},
-    {"firstName":"Yoni", "lastName":"Nezer", "booksSold":103, "age":26},
-    {"firstName":"Ronny", "lastName":"Cabara", "booksSold":230, "age":28},
-    {"firstName":"Shachar", "lastName":"Romi", "booksSold":75, "age":24},
-    {"firstName":"Arya", "lastName":"Starks‬‏", "booksSold":311, "age":18},
-    {"firstName":"Cersei", "lastName":"Lannister", "booksSold":61, "age":41}
+    {"firstName":"Ster", "lastName":"War", "booksSold":108, "age":56},
+    {"firstName":"Shachar", "lastName":"Romi", "booksSold":98, "age":24},
+    {"firstName":"Sharon", "lastName":"Romi", "booksSold":87, "age":25},
+    
 
 ];
 
 
 var books = [
 
-    {"id":1, "name":"Game Of Thrones", "author":"George R. R. Martin", "genre":"Fantasy, political strategy, epic fantasy", "price":160, "title":"easy" },
-    {"id":2, "name":"The Hunger Games", "author":"Suzanne Collins", "genre":"Adventure, science fiction, drama, action", "price":120, "title":"medium" },
-    {"id":3, "name":"The Fault in Our Stars", "author":"John Green", "genre":"Realistic, fiction", "price":80, "title":"hard" },
-    {"id":4, "name":"The Elephant Tree", "author":" R.D. Ronald", "genre":"Rock, Blues, Doom, Stoner with a garnish of parsley", "price":120, "title":"easy" }
+    {"id":1, "name":"Game Of Thrones"} 
+    {"id":2, "name":"pulp fiction"}
+    {"id":3, "name":"kill bill"}
+    {"id":4, "name":"fear and loathing in las vegas"}
     
 ];
 
 
-var members = [
 
-    {"firstName":"Ned", "lastName":"Starks", "memberSince":1998, "age":56},
-    {"firstName":"Shlomi", "lastName":"Yosef", "memberSince":2012, "age":24},
-    {"firstName":"David", "lastName":"Halfon", "memberSince":2003, "age":25},
-    {"firstName":"Jon", "lastName":"Snow", "memberSince":2008, "age":30},
-    {"firstName":"Cersei", "lastName":"Lannister", "memberSince":1988, "age":41}
-    
-];
+
 
 
 
